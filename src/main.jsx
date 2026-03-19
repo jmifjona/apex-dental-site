35a70263c37178e6eac57e4ff1bae7cbe0c07596
jmifjona <145934876+jmifjona@users.noreply.github.com>
2026-03-19 20:21:07 +0100

    Refactor main.jsx to use BrowserRouter and ApexDentalWebsite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import ApexDentalWebsite from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(
+  <BrowserRouter>
+    <ApexDentalWebsite />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
