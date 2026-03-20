609e7e876d36a39ccd3388d9db9ddb9b71f22a52
jmifjona <145934876+jmifjona@users.noreply.github.com>
2026-03-20 22:35:36 +0100

    Refactor main.jsx to use createRoot and StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
-import ApexDentalWebsite from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <ApexDentalWebsite />
-  </BrowserRouter>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
