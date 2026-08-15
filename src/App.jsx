d41bdc2807c7996067c73da1c00f34b67192cac9
jmifjona <145934876+jmifjona@users.noreply.github.com>
2026-08-15 20:41:26 +0200

    Fix nav link href to contact page questionnaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-  import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, Link, useLocation } from 'react-router-dom';
 import { useForm, ValidationError } from '@formspree/react';
 import GoogleAdsAppPage from './GoogleAdsAppPage';
